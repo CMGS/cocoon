3d64562f45e38610511552fd308e22ca7894803b
CMGS <ilskdw@gmail.com>
2026-02-19 13:35:48 +0800

    phase2: add OCI runtime workspace and overlay mount manager

---
+++ vm/engine/overlay_runtime_linux.go
@@ -0,0 +1,29 @@
+//go:build linux
+
+package engine
+
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+func overlayRuntimeSupported() bool {
+	return true
+}
+
+func overlayMount(source, target, fstype string, flags uintptr, data string) error {
+	return unix.Mount(source, target, fstype, flags, data)
+}
+
+func overlayUnmount(target string, flags int) error {
+	return unix.Unmount(target, flags)
+}
+
+func overlayReadMountInfo() ([]byte, error) {
+	return os.ReadFile("/proc/self/mountinfo")
+}
+
+func overlayUnmountFlags() int {
+	return unix.MNT_DETACH
+}
---
+++ vm/engine/overlay_runtime_other.go
@@ -0,0 +1,23 @@
+//go:build !linux
+
+package engine
+
+func overlayRuntimeSupported() bool {
+	return false
+}
+
+func overlayMount(source, target, fstype string, flags uintptr, data string) error {
+	return nil
+}
+
+func overlayUnmount(target string, flags int) error {
+	return nil
+}
+
+func overlayReadMountInfo() ([]byte, error) {
+	return nil, nil
+}
+
+func overlayUnmountFlags() int {
+	return 0
+}
